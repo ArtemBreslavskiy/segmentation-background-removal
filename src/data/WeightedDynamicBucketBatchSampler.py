94c486b68008644da6f194f46c11e730c756b924
ArtemBreslavskiy <artm.breslavskiy@gmail.com>
2026-08-18 02:33:36 +0500

    refactor: simplified WeightedDynamicBucketBatchSampler

--- src/data/WeightedDynamicBucketBatchSampler.py
+++ src/data/WeightedDynamicBucketBatchSampler.py
@@ -1,16 +1,14 @@
-import warnings
-from typing import List
-
 import numpy as np
+import warnings
 from torch.utils.data import WeightedRandomSampler
 
 
 class WeightedDynamicBucketBatchSampler(WeightedRandomSampler):
     def __init__(
         self,
-        weights: List[float],
-        dataset_areas: List[int],
-        dataset_aspect_ratios: List[float],
+        weights: list[float],
+        dataset_areas: list[int],
+        dataset_aspect_ratios: list[float],
         shuffle: bool = True,
         max_batch_size: int = 32,
         min_batch_size: int = 1,
@@ -19,18 +17,10 @@ class WeightedDynamicBucketBatchSampler(WeightedRandomSampler):
         skip_overload_examples: bool = True,
         send_overload_report: bool = True,
     ):
-        if max_batch_size < 1:
-            raise ValueError("max_batch_size must be >= 1")
-        if min_batch_size < 1:
-            raise ValueError("min_batch_size must be >= 1")
-        if max_load < 1:
-            raise ValueError("max_load must be >= 1")
-        if max_batch_size < min_batch_size:
-            raise ValueError("max_batch_size cannot be less than min_batch_size")
-        if len(weights) == 0:
-            raise ValueError("Empty dataset is not allowed")
-        if not len(weights) == len(dataset_areas) == len(dataset_aspect_ratios):
-            raise ValueError("The length of the weights, dataset_areas, and dataset_aspect_ratio lists does not match")
+        self._validate_weights(weights)
+        self._validate_batch_sizes(max_batch_size, min_batch_size)
+        self._validate_max_load(max_load)
+        self._validate_lengths(weights, dataset_areas, dataset_aspect_ratios)
 
         super().__init__(weights, len(weights), replacement=replacement)
         self.dataset_areas = np.array(dataset_areas)
@@ -41,7 +31,30 @@ class WeightedDynamicBucketBatchSampler(WeightedRandomSampler):
         self.shuffle = shuffle
         self.send_overload_report = send_overload_report
         self.skip_overload_examples = skip_overload_examples
-        self.length = len(weights)
+
+    @staticmethod
+    def _validate_weights(weights) -> None:
+        if len(weights) == 0:
+            raise ValueError("Empty dataset is not allowed")
+
+    @staticmethod
+    def _validate_batch_sizes(max_batch_size: int, min_batch_size: int) -> None:
+        if max_batch_size < 1:
+            raise ValueError("max_batch_size must be >= 1")
+        if min_batch_size < 1:
+            raise ValueError("min_batch_size must be >= 1")
+        if max_batch_size < min_batch_size:
+            raise ValueError("max_batch_size cannot be less than min_batch_size")
+
+    @staticmethod
+    def _validate_max_load(max_load: int) -> None:
+        if max_load < 1:
+            raise ValueError("max_load must be >= 1")
+
+    @staticmethod
+    def _validate_lengths(weights: list[float], dataset_areas: list[int], dataset_aspect_ratios: list[float]) -> None:
+        if not len(weights) == len(dataset_areas) == len(dataset_aspect_ratios):
+            raise ValueError("The length of the weights, dataset_areas, and dataset_aspect_ratio lists does not match")
 
     def __iter__(self):
         sampled_indices = np.array(list(super().__iter__()))
@@ -53,36 +66,43 @@ class WeightedDynamicBucketBatchSampler(WeightedRandomSampler):
         areas = areas[order]
 
         batches = []
-        i = 0
+        current_index = 0
         missing = 0
-        while i < len(sampled_indices):
+
+        while current_index < len(sampled_indices):
             current_load = 0
             batch = []
-            while i < len(sampled_indices):
+
+            while current_index < len(sampled_indices):
                 if len(batch) >= self.max_batch_size:
                     break
-                if current_load + areas[i] > self.max_load:
+
+                if current_load + areas[current_index] > self.max_load:
                     if len(batch) == 0:
-                        if self.skip_overload_examples:
-                            i += 1
-                            missing += 1
-                            continue
-                        elif self.send_overload_report:
+                        if self.send_overload_report:
                             warnings.warn(
                                 f"An sample was found that exceeds the limits"
-                                f" even with a batch size of 1 {areas[i]} > {self.max_load}"
+                                f" even with a batch size of 1 {areas[current_index]} > {self.max_load}"
                             )
+                        if self.skip_overload_examples:
+                            current_index += 1
+                            missing += 1
+                            continue
+
                     elif len(batch) < self.min_batch_size:
                         warnings.warn(
                             f"The minimum size batch exceeded the limits, min_batch_size:" f" {self.min_batch_size}"
                         )
                     else:
                         break
-                current_load += areas[i]
-                batch.append(sampled_indices[i])
-                i += 1
+
+                current_load += areas[current_index]
+                batch.append(sampled_indices[current_index])
+                current_index += 1
+
             if batch:
                 batches.append(batch)
+
         if self.send_overload_report and self.skip_overload_examples and missing != 0:
             warnings.warn(f"{missing} samples were skipped")
 
