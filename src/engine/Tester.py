3de36f98d27de6428da3d261b0aad65d16e6568b
ArtemBreslavskiy <artm.breslavskiy@gmail.com>
2026-08-20 23:56:11 +0500

    refactor: simplify Tester

--- src/engine/Tester.py
+++ src/engine/Tester.py
@@ -1,34 +1,36 @@
 import logging
-from datetime import datetime
-from pathlib import Path
-from typing import Callable, Dict, Optional, Union
-
 import torch
 import torch.nn as nn
 import torch.utils.data as data
 import torchmetrics
-
+from datetime import datetime
+from pathlib import Path
+from typing import Callable
 from src.engine.BaseModule import BaseModule
 from src.utils.factories.loss_fn_factory import create_loss
 from src.utils.factories.metrics_factory import create_metrics
 from src.utils.factories.model_factory import create_model
+from src.configs.schemas.model.model import ModelConfig
+from src.configs.schemas.learning.learning import LearningConfig
 
 
 class Tester(BaseModule):
     def __init__(
         self,
         model: nn.Module,
-        config: Dict,
-        loss_function: Union[nn.Module, Callable],
-        metrics: Optional[Dict[str, torchmetrics.Metric]] = None,
-        log_dir: Optional[Union[str, Path]] = None,
-        device: Optional[Union[torch.device, str]] = None,
-        model_name: Optional[str] = None,
-        logger: Optional[logging.Logger] = None,
+        model_config: ModelConfig,
+        learning_config: LearningConfig,
+        loss_function: nn.Module | Callable,
+        metrics: dict[str, torchmetrics.Metric] | None = None,
+        log_dir: str | Path | None = None,
+        device: torch.device | str | None = None,
+        model_name: str | None = None,
+        logger: logging.Logger | None = None,
     ):
         super().__init__(
             model=model,
-            config=config,
+            model_config=model_config,
+            learning_config=learning_config,
             loss_function=loss_function,
             log_dir=log_dir,
             metrics=metrics,
@@ -37,27 +39,11 @@ class Tester(BaseModule):
             logger=logger,
         )
 
-    def _validate_test_dataloader(self, dataloader):
-        if dataloader is None:
-            error_msg = "test_dataloader cannot be none"
-            self.logger.exception(error_msg)
-            raise ValueError(error_msg)
-        if not isinstance(dataloader, data.DataLoader):
-            error_msg = "Unsupported test_dataloader type"
-            self.logger.exception(error_msg)
-            raise ValueError(error_msg)
-        if len(dataloader.dataset) == 0:
-            error_msg = "test_dataloader dataset cannot be empty"
-            self.logger.exception(error_msg)
-            raise ValueError(error_msg)
-        return dataloader
-
     def evaluate(
         self,
         dataloader: data.DataLoader,
-        metrics: Optional[Dict[str, torchmetrics.Metric]] = None,
-        tqdm_mode: Optional[str] = "default",
-    ) -> Dict[str, float]:
+        metrics: dict[str, torchmetrics.Metric] | None = None,
+    ) -> dict[str, float]:
         self._validate_dataloader(dataloader)
 
         if metrics is None:
@@ -69,15 +55,14 @@ class Tester(BaseModule):
         self.logger.info(f"Starting evaluation on {len(dataloader)} batches")
         self.logger.debug(f"Using metrics: {list(metrics.keys())}")
 
-        metrics_values = self.run_epoch(dataloader, "test", metrics, tqdm_mode=tqdm_mode)
+        metrics_values = self.run_epoch(dataloader, "test", metrics)
 
         self.logger.info("Evaluation completed")
-
         self._save_metrics(metrics_values)
         self.logger.debug("Results saved")
         return metrics_values
 
-    def _save_metrics(self, metrics: Dict):
+    def _save_metrics(self, metrics: dict):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         filename = self.log_dir / f"{self.model_name}_test_{self.current_epoch+1}_{timestamp}.pt"
         torch.save(metrics, filename)
@@ -86,25 +71,29 @@ class Tester(BaseModule):
     @classmethod
     def load_tester(
         cls,
-        path: Union[str, Path],
-        log_dir: Optional[Union[str, Path]] = None,
-        device: Optional[Union[torch.device, str]] = None,
-        logger: Optional[logging.Logger] = None,
+        path: str | Path,
+        log_dir: str | Path | None = None,
+        device: torch.device | str | None = None,
+        logger: logging.Logger | None = None,
     ):
         checkpoint = torch.load(path, map_location=device, weights_only=False)
 
-        config = checkpoint["config"]
-        if config is None or len(config) == 0:
-            raise ValueError("Checkpoint does not contain config. Cannot restore components.")
+        model_config = checkpoint["model_config"]
+        model_config = BaseModule._validate_config(model_config)
+        learning_config = checkpoint["learning_config"]
+        learning_config = BaseModule._validate_config(learning_config)
+        evaluating_config = checkpoint["evaluating_config"]
+        evaluating_config = BaseModule._validate_config(evaluating_config)
 
-        model = create_model(config)
-        loss_function = create_loss(config)
-        metrics = create_metrics(config)
+        model = create_model(model_config)
+        loss_function = create_loss(learning_config)
+        metrics = create_metrics(evaluating_config)
 
         tester = cls(
             model=model,
             loss_function=loss_function,
-            config=config,
+            model_config=model_config,
+            learning_config=learning_config,
             log_dir=log_dir,
             metrics=metrics,
             device=device,
